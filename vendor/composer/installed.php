7d3756c595a412c7f106e39aa6fe52a0e63270e0
Jared R. Clemence <Jared R. Clemence@JRC_2024_Dell>
2025-01-02 21:35:34 -0800

    Connected to github.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jaredclemence/imap',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '94a1de30a8c4d114864918d941f6eb528974997f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jaredclemence/imap' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '94a1de30a8c4d114864918d941f6eb528974997f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
