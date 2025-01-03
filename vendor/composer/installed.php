c080a047603035178f0d957a77e57c3f5eafa5e2
Jared R. Clemence <Jared R. Clemence@JRC_2024_Dell>
2025-01-02 22:08:04 -0800

    Update composer to work with packagist.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'jaredclemence/imap',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '94a1de30a8c4d114864918d941f6eb528974997f',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7d3756c595a412c7f106e39aa6fe52a0e63270e0',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'jaredclemence/imap' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '94a1de30a8c4d114864918d941f6eb528974997f',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7d3756c595a412c7f106e39aa6fe52a0e63270e0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
